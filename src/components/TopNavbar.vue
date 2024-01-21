504825847b10e0f93ce80347bdc2de782da099cc
Bear Codes <bearlovestocode@gmail.com>
2024-01-21 09:19:54 +0000

    added templates

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-<div class="navbar bg-base-100">
+  <div class="navbar bg-base-100">
     <div class="flex-1">
       <a class="btn btn-ghost text-xl">daisyUI</a>
     </div>
@@ -12,10 +12,16 @@
       <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
-            <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img
+              alt="Tailwind CSS Navbar component"
+              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
           </div>
         </div>
-        <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <ul
+          tabindex="0"
+          class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+        >
           <li>
             <a class="justify-between">
               Profile
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DefaultView from '../views/DefaultView.vue'
+import MovieSearch from '../views/MovieSearch.vue'
+import MovieInfo from '../views/MovieInfo.vue'
+import WatchMovie from '../views/WatchMovie.vue'
+import PrivacyPolicy from '../views/PrivacyPolicy.vue'
+import ToS from '../views/ToS.vue'
+import ErrorPage from '../views/ErrorPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +14,36 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: DefaultView
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: MovieSearch
+    },
+    {
+      path: '/info',
+      name: 'info',
+      component: MovieInfo
+    },
+    {
+      path: '/watch',
+      name: 'watch',
+      component: WatchMovie
+    },
+    {
+      path: '/policy',
+      name: 'privacy policy',
+      component: PrivacyPolicy
+    },
+    {
+      path: '/tos',
+      name: 'tos',
+      component: ToS
+    },
+    {
+      path: '/error',
+      name: 'error',
+      component: ErrorPage
     }
   ]
 })
