a26985a31fcc97fbb40ca3ad1668f42a60bd60ef
Bear Codes <bearlovestocode@gmail.com>
2024-01-21 06:44:53 +0000

    added daisyui

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import TopNavbar from './components/TopNavbar.vue'
+import BottomFooter from './components/BottomFooter.vue'
 </script>
 
 <template>
   <header></header>
+  <TopNavbar />
   <RouterView />
+  <BottomFooter />
 </template>
